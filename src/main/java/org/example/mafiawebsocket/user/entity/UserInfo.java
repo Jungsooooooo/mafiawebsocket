c4b84929ddc6b4904a21f19bba0f28d743e32976
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2024-01-30 18:41:56 +0900

    user 생성을 위한 builder 설정

--- src/main/java/org/example/mafiawebsocket/user/entity/UserInfo.java
+++ src/main/java/org/example/mafiawebsocket/user/entity/UserInfo.java
@@ -1,13 +1,18 @@
 package org.example.mafiawebsocket.user.entity;
 
+import ch.qos.logback.classic.spi.LoggingEventVO;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Entity
 @Getter
 @Table(name = "userInfo")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserInfo {
 
     @Id
@@ -19,5 +24,11 @@ public class UserInfo {
     private String name;
     private String password;
 
+    @Builder
+    public UserInfo(String name, String password){
+        this.name       = name;
+        this.password   = password;
+        this.uuid       = UUID.randomUUID();
+    }
 
 }
