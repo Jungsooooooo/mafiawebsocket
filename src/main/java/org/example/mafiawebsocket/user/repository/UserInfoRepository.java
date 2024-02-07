a7affa57485a0b7dab00ff5639a19e347d43e56d
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2024-02-07 16:32:11 +0900

    name과 비밀번호 로그인

--- src/main/java/org/example/mafiawebsocket/user/repository/UserInfoRepository.java
+++ src/main/java/org/example/mafiawebsocket/user/repository/UserInfoRepository.java
@@ -7,4 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
 
+    UserInfo findByName(String name);
+
+    UserInfo findByNameAndPassword(String name, String password);
 }
