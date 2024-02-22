61889ff6c1180f3c6d44afab0fcbece2954fc444
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2024-02-22 13:53:04 +0900

    jwt 작업을 위한 class 추가

--- src/main/java/org/example/mafiawebsocket/config/SecurityConfig.java
+++ src/main/java/org/example/mafiawebsocket/config/SecurityConfig.java
@@ -1,40 +1,58 @@
 package org.example.mafiawebsocket.config;
 
+import lombok.RequiredArgsConstructor;
+import org.example.mafiawebsocket.token.JwtAccessDeniedHandler;
+import org.example.mafiawebsocket.token.JwtAuthenticationEntryPoint;
+import org.example.mafiawebsocket.token.TokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig{
 
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+                .csrf().disable()
 
-        http.securityMatcher("/**")
-                .authorizeHttpRequests(authorize -> authorize.
-                        requestMatchers("/api/users/login").permitAll()
-                        .anyRequest().authenticated()).formLogin((formLogin) -> formLogin
-                        .loginProcessingUrl("/api/users/login")
-                        .defaultSuccessUrl("/all").permitAll())
-                .logout((logout) -> logout
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/api/users/logout"))
-                        .logoutSuccessUrl("/test")
-                        .invalidateHttpSession(true))
-                .csrf(csrf-> csrf.disable());
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
 
-        return http.build();
+                // enable h2-console
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeHttpRequests() // HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정하겠다.
+                .requestMatchers("/api/users/login").permitAll() // 로그인 api
+                .requestMatchers("/api/users/join").permitAll()
+                .anyRequest().authenticated() // 그 외 인증 없이 접근X
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider)); // JwtFilter를 addFilterBefore로 등록했던 JwtSecurityConfig class 적용
+
+        return httpSecurity.build();
     }
 
     @Bean
