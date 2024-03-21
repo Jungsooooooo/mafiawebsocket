b62960c6447aeb962514c8079a8ca3bebacea727
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2024-03-21 15:32:41 +0900

    websocket 방 만들기 및 채팅 기능 구현

---
+++ src/main/java/org/example/mafiawebsocket/websocket/controller/ChatController.java
@@ -0,0 +1,29 @@
+package org.example.mafiawebsocket.websocket.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.mafiawebsocket.websocket.dto.ChatRoom;
+import org.example.mafiawebsocket.websocket.service.ChatService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/chat")
+public class ChatController {
+
+    private final ChatService service;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam String name){
+        return service.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRooms(){
+        return service.findAllRoom();
+    }
+}
+
